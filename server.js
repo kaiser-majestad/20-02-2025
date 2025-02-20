7c7d0671f988b783f28ab99fb5437a6654d0ee2e
kaiser-majestad <nelsonalzateg@donbosco.edu.co>
2025-02-20 18:19:18 -0500

    cambios

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
-const eventosRouter = require('../Santa Maria/gestion_eventos/router/eventos');
-const inscripcionesRouter = require('../Santa Maria/gestion_eventos/router/inscripciones');
-const usuariosRouter = require('../Santa Maria/gestion_eventos/router/usuarios');
+
+
+const eventosRouter = require('./gestion_eventos/router/eventos');
+const inscripcionesRouter = require('./gestion_eventos/router/inscripciones'); 
+const usuariosRouter = require('./gestion_eventos/router/usuarios');
+
 
 app.use(express.json());
 
+
 app.use('/eventos', eventosRouter);
-app.use('/inscripciones', inscripcionesRouter);
+app.use('/inscripciones', inscripcionesRouter); 
 app.use('/usuarios', usuariosRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`); 
 });
